1deda2e10c199538e61b06738a812fa9ca425922
Vivek Kumar <rk77610792@gmail.com>
2026-04-17 10:41:34 +0530

    Update Rag_App.py

--- Rag_App.py
+++ Rag_App.py
@@ -20,11 +20,11 @@ os.environ["USER_AGENT"] = "rag-app"
 
 # ------------------ LOAD ENV ------------------
 load_dotenv()
-groq_api_key = st.secrets["GROQ_API_KEY"]
+groq_api_key = st.secrets["first_pr"]
 
 # ------------------ UI ------------------
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
-st.image("PragyanAI_Transperent.png")
+st.image("sc.jpg")
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 
 # ------------------ SESSION STATE ------------------
